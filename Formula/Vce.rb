d465384c229ebba51542a9b9b33dd6849939e1ee
Randy Lutcavich <randy.lutcavich@thisisartium.com>
2024-01-11 00:25:00 -0600

    feat(release): Updated url and sha for new release

--- Formula/Vce.rb
+++ Formula/Vce.rb
@@ -1,8 +1,8 @@
 class Vce < Formula
   desc "Randroid's Text to Voice Script. Using OpenAI's API."
   homepage "https://github.com/randroid88/homebrew-vce"
-  url "https://github.com/randroid88/homebrew-vce/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "6f3373152c8e4e98a02a4bd9eec947fb5891209e7c0dcce40d564dfd720d628c"
+  url "https://github.com/randroid88/homebrew-vce/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "dc12c909b9726dbfac883911a6d045ac75c0fa470541c3f3220db9862efab61d"
 
   def install
     libexec.install Dir["*.py"]
