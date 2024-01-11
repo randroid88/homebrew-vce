2546b4e329cfb9d4467306aaab3280e914ec145b
Randy Lutcavich <randy.lutcavich@thisisartium.com>
2024-01-11 00:42:52 -0600

    fix(installation): Making sure to load both python scripts into libexec when installed through homebrew

--- Formula/Vce.rb
+++ Formula/Vce.rb
@@ -4,10 +4,16 @@ class Vce < Formula
   url "https://github.com/randroid88/homebrew-vce/archive/refs/tags/1.0.1.tar.gz"
   sha256 "dc12c909b9726dbfac883911a6d045ac75c0fa470541c3f3220db9862efab61d"
 
+  # uses_from_macos "python@3"
+
   def install
     libexec.install Dir["*.py"]
-    bin.install "vce.py" => "vce"
-    chmod 0555, bin/"vce"
+
+    # Write a wrapper script and install it to bin
+    (bin/"vce").write <<~EOS
+      #!/bin/bash
+      /usr/bin/env python3 "#{libexec}/vce.py" "$@"
+    EOS
   end
 
   test do
--- vce.py
+++ vce.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python3
 import os
 import sys
-from contextlib import contextmanager
 
-PATH_INDEX = 0
-LOCAL_MODULE_PATH = './'
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "libexec"))
-
-
-@contextmanager
-def extend_sys_path(path):
-    sys.path.insert(PATH_INDEX, path)
-    try:
-        yield
-    finally:
-        sys.path.pop(PATH_INDEX)
-
+if os.path.dirname(os.path.realpath(__file__)) != os.getcwd():
+    # If current working directory is not where the file lives, we should load files from libexec
+    LIBEXEC_PATH = os.path.join(os.path.dirname(__file__), "..", "libexec")
+    sys.path.insert(0, LIBEXEC_PATH)
 
 if __name__ == '__main__':
+    print(os.path.dirname(os.path.realpath(__file__)) )
+    print(os.getcwd())
     command_line_args = sys.argv[1:]
     try:
-        with extend_sys_path(LOCAL_MODULE_PATH):
-            import vce_lib
-    except ModuleNotFoundError:
         import vce_lib
-
+    except ModuleNotFoundError:
         print('\nModuleNotFoundError')
+        sys.exit(1)
     except KeyboardInterrupt:
         print('\nProgram interrupted.')
         sys.exit(1)
