79061257fb7fae7906b3549705b268a14ac1bcf8
Randy Lutcavich <randy.lutcavich@thisisartium.com>
2024-01-10 23:56:47 -0600

    feat(release): Updated formula

--- Formula/Vce.rb
+++ Formula/Vce.rb
@@ -1,8 +1,8 @@
 class Vce < Formula
   desc "Randroid's Text to Voice Script. Using OpenAI's API."
   homepage "https://github.com/randroid88/homebrew-vce"
-  url "https://github.com/randroid88/homebrew-vce/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 ""
+  url "https://github.com/randroid88/homebrew-vce/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "6f3373152c8e4e98a02a4bd9eec947fb5891209e7c0dcce40d564dfd720d628c"
 
   def install
     bin.install "vce.py" => "vce"
