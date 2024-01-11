3d6f1c631ad16a5dbb9a626f6e320753fc261d6f
Randy Lutcavich <randy.lutcavich@thisisartium.com>
2024-01-11 00:22:33 -0600

    fix(installation): Now making sure vce_lib.py is installed when setup through brew tap, but also still supporting local development.

--- Formula/Vce.rb
+++ Formula/Vce.rb
@@ -5,6 +5,7 @@ class Vce < Formula
   sha256 "6f3373152c8e4e98a02a4bd9eec947fb5891209e7c0dcce40d564dfd720d628c"
 
   def install
+    libexec.install Dir["*.py"]
     bin.install "vce.py" => "vce"
     chmod 0555, bin/"vce"
   end
--- vce.py
+++ vce.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
+import os
 import sys
+from contextlib import contextmanager
+
+PATH_INDEX = 0
+LOCAL_MODULE_PATH = './'
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "libexec"))
+
+
+@contextmanager
+def extend_sys_path(path):
+    sys.path.insert(PATH_INDEX, path)
+    try:
+        yield
+    finally:
+        sys.path.pop(PATH_INDEX)
+
 
 if __name__ == '__main__':
+    command_line_args = sys.argv[1:]
     try:
+        with extend_sys_path(LOCAL_MODULE_PATH):
+            import vce_lib
+    except ModuleNotFoundError:
         import vce_lib
 
-        args = sys.argv[1:]
-        vce_lib.main(args)
+        print('\nModuleNotFoundError')
     except KeyboardInterrupt:
-        print('\n Program interrupted.')
+        print('\nProgram interrupted.')
         sys.exit(1)
+    else:
+        vce_lib.main(command_line_args)
